5edbf99c1875ad0deaac18a4c682167d765c12d0
Anunay Choudhury <64680716+Anunay16@users.noreply.github.com>
2021-12-30 23:05:11 +0530

    Create DJIKSTRA's Algorithm.cpp

---
+++ DJIKSTRA's Algorithm.cpp
@@ -0,0 +1,82 @@
+// link to the problem: https://www.hackerrank.com/challenges/dijkstrashortreach/problem
+
+//Dijkstra's Algorithm
+#include <bits/stdc++.h>
+using namespace std;
+#define pi pair<int, int>
+
+struct Edge {
+    int u, v, wt;
+    Edge(int _u, int _v, int _wt) {
+        u = _u;
+        v = _v;
+        wt = _wt;
+    }
+};
+
+void dijkstras(vector<vector<Edge>>& adj, vector<bool>& visited, vector<int>& shortestPaths, int src) {
+    //make a priority queue: min heap
+    priority_queue<pi, vector<pi>, greater<pi>> pq; // <wt, vertex>
+    pq.push({0, src});
+    while (!pq.empty()) {
+        //remove
+        pi rem = pq.top();
+        pq.pop();
+        int wt = rem.first;
+        int vtx = rem.second;
+        //mark*
+        if (visited[vtx]) continue; // cycle detected
+        visited[vtx] = true;
+        //work
+        shortestPaths[vtx] = wt;
+        //add*
+        for (auto &edge: adj[vtx]) {
+            if (!visited[edge.v]) {
+                pq.push({wt + edge.wt, edge.v});
+            }
+        }
+    }
+}
+
+void solve() {
+    int V, E;
+    cin >> V >> E;
+    // make the adjacency list
+    vector<vector<Edge>> adj(V + 1);
+    for (int i = 0; i < E; i++) {
+        int u, v, wt;
+        cin >> u >> v >> wt;
+        adj[u].push_back(Edge(u, v, wt));
+        adj[v].push_back(Edge(v, u, wt));
+    }
+    int src;
+    cin >> src;
+    vector<bool> visited(V + 1, false);
+    vector<int> shortestPaths(V + 1, -1);
+    dijkstras(adj, visited, shortestPaths, src);
+    for (int i = 1; i < V + 1; i++) {
+        if (i != src) {
+            cout << shortestPaths[i] << " ";
+        }
+    }
+    cout << "\n";
+}
+
+void FIO()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main()
+{
+    FIO();
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
